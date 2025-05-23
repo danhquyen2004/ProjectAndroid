4c3e46d53014014f45b06c3e7c81c7227dbab8ba
Huuwx <chumanhhuu2@gmail.com>
2025-05-24 01:25:16 +0700

    add activity_solo_rank

---
+++ Android/app/src/main/java/com/example/tlupickleball/adapters/PlayerAdapter.java
@@ -0,0 +1,55 @@
+package com.example.tlupickleball.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.tlupickleball.R;
+import com.example.tlupickleball.model.Player;
+
+import java.util.List;
+
+public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.PlayerViewHolder> {
+    private List<Player> playerList;
+
+    public PlayerAdapter(List<Player> playerList) {
+        this.playerList = playerList;
+    }
+
+    @NonNull
+    @Override
+    public PlayerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_player, parent, false);
+        return new PlayerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlayerViewHolder holder, int position) {
+        Player player = playerList.get(position);
+        holder.txtName.setText(player.getName());
+        holder.txtScore.setText(String.valueOf(player.getSoloPoint()));
+        holder.imgAvatar.setImageResource(player.getAvatarResourceId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return playerList.size();
+    }
+
+    public static class PlayerViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgAvatar;
+        TextView txtName, txtScore;
+
+        public PlayerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgAvatar = itemView.findViewById(R.id.imgAvatar);
+            txtName = itemView.findViewById(R.id.txtName);
+            txtScore = itemView.findViewById(R.id.txtScore);
+        }
+    }
+}
